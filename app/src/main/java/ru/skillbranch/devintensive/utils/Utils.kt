0e7528ff70d13c9bfcea91cbc0ad3873a9bfb6e3
kostya <kostya.krsk@gmail.com>
2020-09-27 22:17:02 +0700

    e3

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -29,7 +29,7 @@ object Utils {
 
     }
 
-    private fun toLatyan(letter: Char) : String{
+    private fun toLatyan(letter: Char?) : String?{
         when(letter){
             'а' -> return "a"
             'А' -> return "A"
